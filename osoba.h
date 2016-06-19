1145a0af01c6d730396f5e47c8571c4de738129e
FeridBesic <ferid.besic@fet.ba>
2016-06-19 00:04:28 +0000

    Dodani operatori za ispis i poredjenje

--- osoba.h
+++ osoba.h
@@ -53,7 +53,20 @@ class Osoba{
 	
 	void printOsoba(){
   std::cout<<_ime<<" "<<_prezime;
-}
+  }
+  
+  std::ostream& operator<<(std::ostream& out)
+  {
+  	out<<this->_ime<<" "<<this->_prezime<<"\t"<<this->_jmbg<<std::endl;
+  }
+  
+  bool operator==(const Osoba& x)
+  {
+  	if(_ime==x._ime && x._prezime==_prezime && x._jmbg==_jmbg)
+  	return true;
+  	else 
+	  return false;
+  }
 };
 
 #endif
